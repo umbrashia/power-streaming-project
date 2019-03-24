83d22c29d6e9a0a2f3e23928a3545009b7b56b57
umbrashia <shantanu34@outlook.com>
2019-03-25 01:06:20 +0530

    data

--- controllers/StreamingController.py
+++ controllers/StreamingController.py
@@ -5,8 +5,13 @@ class StreamingController(system.FlaskServerBinder):
     def internalRouting(self):
         if self.subModule=="hello":
             return self.postStream()
+        if self.subModule=="streamingView":
+            return self.streamingView()
         return flask.Response(response=None,status=404)
     
     def postStream(self):
         t=5+10
         return "done again : "+str(t)
+        
+    def streamingView(self):
+        return flask.render_template("streamingVideoSender.html")
