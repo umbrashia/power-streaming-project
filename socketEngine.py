bc8f7dcb9b53d0fb20bb7ededd235b97184fabd1
umbrashia <shantanu34@outlook.com>
2019-04-01 07:54:30 +0530

    transfer bufer and append them

--- socketEngine.py
+++ socketEngine.py
@@ -1,13 +1,22 @@
 import asyncio
 import websockets
+import json
 
 
 async def hello(websocket, path):
     while True:
-        name = await websocket.recv()
-        
-        print(f"< {name}")
-        greeting = f"Hello {name}!"
+        temp = await websocket.recv()
+        myJson=json.dumps(temp)
+        # print(f"< {name}")
+        bn=[]
+        for kk in myJson['data']:
+            bn.append(float(kk))
+        # bn=list(map(float,myJson['data']))
+        bi=bytearray(bn)
+        f = open('static/temp.wav', 'ab')
+        f.write(bi)
+        f.close()
+        greeting = f"Hello {myJson['itration']}!"
 
         await websocket.send(greeting)
         print(f"> {greeting}")
