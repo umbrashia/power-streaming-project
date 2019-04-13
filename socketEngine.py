02f68a89422d4e0c39f694cdea7a5cf31b792169
umbrashia <shantanu34@outlook.com>
2019-04-13 15:40:54 +0530

    Pure Video Is converted...

--- socketEngine.py
+++ socketEngine.py
@@ -1,21 +1,18 @@
 import asyncio
 import websockets
 import json
+import kafka
 
 
 async def hello(websocket, path):
+    proCall=kafka.KafkaProducer()
+    
     while True:
         temp = await websocket.recv()
         myJson=json.loads(temp)
         # print(f"< {name}")
-        bn=[]
-        for kk in myJson['data'].split(","):
-            bn.append(kk)
-        # bn=list(map(float,myJson['data']))
-        bi=bytearray(myJson['data'],encoding='utf8')
-        f = open('static/'+str(myJson['itration'])+'_temp.wav', 'wb')
-        f.write(bi)
-        f.close()
+        proCall.send(topic="radha",value=myJson['binary'])
+        proCall.flush()
         greeting = f"Hello {myJson['itration']}!"
 
         await websocket.send(greeting)
@@ -26,3 +23,10 @@ start_server = websockets.serve(hello, 'localhost', 8765)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
+"""bn=[]
+for kk in myJson['data'].split(","):
+    bn.append(kk)
+bi=bytearray(myJson['data'],encoding='utf8')
+f = open('static/'+str(myJson['itration'])+'_temp.wav', 'wb')
+f.write(bi)
+f.close()"""
